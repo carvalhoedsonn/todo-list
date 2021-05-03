6bdbb7b90a2149cbb4295dd582c265e863d1ce75
ejscf <edsonjscf@gmail.com>
2021-05-03 20:47:39 -0300

    :tada: First commit

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>
+          Today's tasks
+        </Text>
+
+        <View style={styles.items}>
+          <Task name="hello"/>
+          <Task name="hello2"/>
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -14,8 +23,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED'
+
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30
+  }
 });
