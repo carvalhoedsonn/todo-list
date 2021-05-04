557f6345462014546d89b247986cf8b4e63755f9
ejscf <edsonjscf@gmail.com>
2021-05-04 09:44:21 -0300

    :white_check_mark: Finished project

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Task from './components/Task'
+import React, { useState } from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  KeyboardAvoidingView, 
+  Platform, 
+  TextInput,
+  TouchableOpacity,
+  Keyboard
+} from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
@@ -11,10 +35,30 @@ export default function App() {
         </Text>
 
         <View style={styles.items}>
-          <Task name="hello"/>
-          <Task name="hello2"/>
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task name={item} />
+                </TouchableOpacity>
+              )
+            })
+          }
         </View>
       </View>
+
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === 'ios' ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a task'} onChangeText={text => setTask(text)}/>
+
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}> 
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
 
     </View>
   );
@@ -36,5 +80,32 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1
   }
 });
---
+++ README.md
@@ -0,0 +1,67 @@
+<div align="center">
+  <img src="./.github/assets/list.png" width="200px" />
+</div>
+
+  # 🗂️ Topics
+   * <a href="#About"> About</a>
+   * <a href="#Layout"> Layout</a>
+   * <a href="#Features"> Features</a>
+   * <a href="#How-to-use"> How to use</a>
+      * <a href="#Prerequisites"> Prerequisites</a>
+      * <a href="#Running-the-app"> Running the app</a>
+   * <a href="#Technologies"> Technologies</a>
+   * <a href="#License"> License</a>
+   * <a href="#Author"> Author</a>
+
+  <h1 id="About">📖 About</h1>
+  <p>I created this simple project to add and remove tasks.</p>
+
+  <h1 id="Layout">💅🏻Layout</h1>
+  <img src="./.github/assets/Layout.png"/>
+
+  <h1 id="Features">📌 Features</h1>
+
+  - [x] Add a task with the name you want
+  - [x] Remove the task you want with just one click.
+  - [ ] Forget some task.
+
+  <h1 id="How-to-use">🤓How to use</h1>
+
+  <h3 id="Prerequisites">Prerequisites</h3>
+
+  <p>To be able to use the application, you need the following tools: </p>
+  <a href="https://git-scm.com"> Git </a> •
+  <a href="https://nodejs.org/en/"> Node.js </a> •
+  <a href="https://expo.io"> Expo </a>
+
+  <h3 id="Running-the-app">Running the app</h3>
+
+  ```bash
+  # Clone this repository
+  $ git clone <https://github.com/ejscf/todo-list.git>
+
+  # Open the APP folder on the terminal
+  $ cd todo-list
+
+  # Install all APP modules
+  $ npm install or yarn install
+
+  # Now just turn on the APP
+  $ expo start
+  ```
+
+  <h1 id="Technologies">💻Technologies</h1>
+
+  <p>The following tools were used in the construction of the project:</p>
+
+  - <a href="https://reactnative.dev/">React Native</a>
+  - <a href="https://www.ecma-international.org">Javascript</a>
+  - <a href="https://expo.io">Expo</a>
+
+  <h1 id="License">📜License</h1>
+
+  ![LICENSE](https://img.shields.io/static/v1?label=LICENSE&message=MIT&color=32b768&logo=rocket-chat)
+    
+  <h1 id="Author">✍🏻Author</h1>
+  <img src="./.github/assets/edson-photo.png" width="110px"/> 
+  <p>Edson Jorge 🦔</p>
